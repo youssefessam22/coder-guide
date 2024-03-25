83114ea55bc442ed889a55ab040064b4502d33f6
youssefessam22 <youseefessamh@gmail.com>
2024-03-25 05:53:03 +0200

    pages added

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ let boxzero = document.querySelector(".elzero");
 let roadsh = document.querySelector("#roadsh")
 
 boxzero.addEventListener('click', function () {
-    window.open("https://www.youtube.com/@ElzeroWebSchool");
+    window.open("refs/youtube-channels/index.html","_self");
 
 });
 
 roadsh.addEventListener('click', function () {
-    window.open("https://roadmap.sh/");
+    window.open("refs/sites/index.html",'_self');
 
 });
 
