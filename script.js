a208731896d13a33df3529162f265ed4f9defbe9
youssef essam <130107867+youssefessam22@users.noreply.github.com>
2024-04-27 22:00:25 +0300

    some touches

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let youtubeChannels = document.querySelector("#youtube-channels");
 let sites = document.querySelector("#sites")
 let softwares = document.querySelector('#softwares')
 let ai = document.querySelector('#ai')
+let search = document.querySelector("#search")
+
 
 youtubeChannels.addEventListener('click', function () {
     window.open("refs/youtube-channels/index.html","_self");
@@ -22,4 +24,8 @@ softwares.addEventListener('click', function () {
 
 ai.addEventListener('click',function () {
     window.open('refs/ai/index.html','_self')
+})
+
+search.addEventListener('click',function () {
+    window.open("search/search.html",'_self')
 })
